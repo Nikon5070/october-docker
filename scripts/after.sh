aa58a48c6b561bf3990844d23d37bbab3025157b
Nikita Kuprin <nikon5070@gmail.com>
2019-06-09 18:28:58 +1000

    init

---
+++ scripts/after.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#check for existence .env
+FILE_ENV=.env
+if [[ ! -f "$FILE_ENV" ]]; then
+    cp .env.example .env
+    #generate key laravel in docker container PHP
+    docker-compose exec -T php php artisan key:generate
+    #generate key ide-helper
+    docker-compose exec -T php php artisan ide-helper:generate
+    #migrate
+    docker-compose exec -T php php artisan migrate
+fi
